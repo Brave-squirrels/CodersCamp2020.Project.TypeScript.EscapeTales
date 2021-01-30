93a7a13f4f513598b4844f1997288997814e629c
Cyprian Zamiara <veryyapeee@gmail.com>
2021-01-30 19:44:18 +0100

    Planning architecture

---
+++ src/boardDOMmanipulation.ts
@@ -0,0 +1,54 @@
+//When we explore field
+const exploredField = ():void =>{
+
+}
+
+//Gain action from STRESS/CLUE
+const gainAction = ():void => {
+
+}
+
+//Get new puzzle
+const newPuzzle = ():void =>{
+
+}
+
+//We have 0 actions
+const noActionPoints = ():void =>{
+
+}
+
+//We solve the puzzle
+const puzzleSolved = ():void =>{
+
+}
+
+//We found a clue
+const fieldClueContent = ():void =>{
+
+}
+
+//Filed has no content
+const fieldNothingContent = ():void =>{
+
+}
+
+//We found a new puzzleCard
+const filedPuzzleContent = ():void =>{
+
+}
+
+//We get evidence from puzzle
+const puzzleEvidenceContent = ():void =>{
+
+}
+
+//We get progress from puzzle
+const puzzleProgressContent = (): void =>{
+
+}
+
+//We get stressCard, gain action and lost evidence
+const stressCard = ():void =>{
+    
+}
