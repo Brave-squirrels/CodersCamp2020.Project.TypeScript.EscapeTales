ac1039a5126f03b0ae136aaf4788feb886ba59d0
Maciej Chojak <maciekch7991@gmail.com>
2021-02-03 16:10:18 +0100

    Change object GameState to class GameState

--- src/state.ts
+++ src/state.ts
@@ -1,28 +1,83 @@
-enum ENDING{
+export enum ENDING{
     HAPPYENDING,
     SADENDING
     }
 
-interface GameState {
-    actionNumbers: number;
-    userParagraphsId: string[];
-    userPuzzlesId: string[];
-    userLocationId: string[];
-    visitedAreasId: string[];
-    storyline: ENDING;
-    userEvidencesId: string[];
+/*
+@Class GameState:
+    Actual game state, include:
+       -number of actions
+       -array of all discovered paragraphes ID
+       -array of all discovered puzzles ID
+       -array of locations ID ???
+       -array of all visited areas ID
+       -number of actual storyline ending 
+       -array of all discovered evidences ID
+
+    To change property use:
+        setNameOfProperty(element)
+
+    To get property use:
+        getNameOfProperty
+*/
+export class GameState {
+    
+    constructor(protected _actionNumbers : number = 5,
+                protected _userParagraphsId : string[] = [],
+                protected _userPuzzlesId : string[] = [], 
+                protected _userLocationId : string[] = [], //i think should be only string
+                protected _visitedAreasId : string[] = [],
+                protected _storyline : ENDING = ENDING.HAPPYENDING, 
+                protected _userEvidencesId : string[] = []
+                ) {  }
+
+
+        set setActionNumbers(newActionNumber:number){
+            this._actionNumbers = newActionNumber;
+        }
+        set setUserParagraphsId(newParagraphsId:string){
+            this._userParagraphsId.push(newParagraphsId);
+        }
+        set setUserPuzzlesId(newPuzzleId:string){
+            this._userPuzzlesId.push(newPuzzleId);
+        }
+        set setUserLocationId(newLocationId:string){
+            this._userLocationId.push(newLocationId);
+        }
+        set setVisitedAreasId(newVisitedAreasId:string){
+            this._visitedAreasId.push(newVisitedAreasId);
+        }
+        set setStoryline(newStoryline:ENDING){
+            this._storyline = newStoryline;
+        }
+        set setUserEvidencesId(newEvidencesId:string){
+            this._userEvidencesId.push(newEvidencesId);
+        }
+
+
+        get getActionsNumbers(){
+            return this._actionNumbers;
+        }
+        get getUserParagraphsId(){
+            return this._userParagraphsId;
+        }
+        get getUserPuzzlesId(){
+            return this._userPuzzlesId;
+        }
+        get getUserLocationId(){
+            return this._userLocationId;
+        }
+        get getVisitedAreasId(){
+            return this._visitedAreasId;
+        }
+        get getStoryline(){
+            return this._storyline;
+        }
+        get getUserEvidencesId(){
+            return this._userEvidencesId;
+        }
+
 }
 
 
-const gameState : GameState = {
-    actionNumbers: 0,
-    userParagraphsId: [],
-    userPuzzlesId: [],
-    userLocationId: [],
-    visitedAreasId: [],
-    storyline: ENDING.HAPPYENDING,
-    userEvidencesId: []
-}
 
-
-module.exports = gameState
