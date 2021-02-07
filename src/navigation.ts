b35c980e338de4cbcc04fae5106adae094b0c2a5
Olek <aleksander.atamanczuk@gmail.com>
2021-02-07 18:27:01 +0100

    refactor: change class to function

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,61 +1,52 @@
 // Render page from template
-class GamePanel {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLElement;
-  element: HTMLElement;
+/*
+    @param {nameId} - id of template to display
+*/
+const createFormTemplate = (nameId: string) => {
+  const templateElement = document.getElementById(
+    nameId
+  ) as HTMLTemplateElement;
 
-  constructor(nameId: string) {
-    this.templateElement = document.getElementById(
-      nameId
-    ) as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app")! as HTMLElement;
+  const hostElement = document.getElementById("app")! as HTMLElement;
+  const importedNode = document.importNode(templateElement.content, true);
 
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
-    this.element = importedNode.firstElementChild as HTMLElement;
+  const element = importedNode.firstElementChild as HTMLElement;
 
-    this.attach(true);
-  }
+  hostElement.insertAdjacentElement("afterbegin", element);
+};
 
-  private attach(insertAtBeginning: boolean) {
-    this.hostElement.insertAdjacentElement(
-      insertAtBeginning ? "afterbegin" : "beforeend",
-      this.element
-    );
-  }
-}
-
-// Navitgation
-const homePanel = new GamePanel("home-page");
-const boardPanel = new GamePanel("game-board");
+createFormTemplate("home-page");
+createFormTemplate("game-board");
 
 const home = document.querySelector(".home")! as HTMLElement;
 const board = document.querySelector(".container")! as HTMLElement;
 const startBtn = document.querySelector("#startBtn")! as HTMLDivElement;
 const homeBackBtn = document.querySelector("#homeBack")! as HTMLDivElement;
 
+// Display page from template
+/*
+    @param {oldElem} - element to hide
+    @param {newElem} - element to show
+*/
+const handleClick = (oldElem: HTMLElement, newElem: HTMLElement) => {
+  oldElem.style.display = "none";
+  oldElem.style.opacity = "0";
+
+  newElem.style.display = "block";
+  setTimeout(() => {
+    newElem.style.opacity = "1";
+  }, 10);
+};
+
+// Navigate between panels
 export default function navigation() {
   // go to board panel
   startBtn.addEventListener("click", () => {
-    home.style.display = "none";
-    home.style.opacity = "0";
-
-    board.style.display = "block";
-    setTimeout(() => {
-      board.style.opacity = "1";
-    }, 10);
+    handleClick(home, board);
   });
 
   // go to home panel
   homeBackBtn.addEventListener("click", () => {
-    board.style.display = "none";
-    board.style.opacity = "0";
-
-    home.style.display = "flex";
-    setTimeout(() => {
-      home.style.opacity = "1";
-    }, 10);
+    handleClick(board, home);
   });
 }
