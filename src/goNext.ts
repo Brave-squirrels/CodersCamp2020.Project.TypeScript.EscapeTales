be4247e2516a6778c2c708eafefdd55baabc2d7c
Cyprian Zamiara <veryyapeee@gmail.com>
2021-02-11 18:12:49 +0100

    refactor: writing next location logic

---
+++ src/goNext.ts
@@ -0,0 +1,5 @@
+//Next location
+//Go to the next location
+//Reset action points
+//Remove all puzzles from previous location
+//Reset progress points
--- src/startGame.ts
+++ src/startGame.ts
@@ -1,7 +1,16 @@
-//Declare all of the objects and arrays
-
 //Take state object from localStorage, if not, create new default
 //Update interface at the beginning
 const initGame = () : void =>{
-
+    //Check if there's state saved in LocalStorage
+    /*
+        If true:
+            -Take the location and go the the current location
+            -Update action points/progressPoints/evidences
+            -Update active puzzles
+            -Update explored area's
+            -Update story book content
+        If false:
+            -declare default gameState object
+            -update actionPoints
+    */
 }
